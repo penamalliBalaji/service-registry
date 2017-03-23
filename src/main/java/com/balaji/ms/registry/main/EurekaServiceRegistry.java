7e6444c4e47e2589cf2c9f9a87688660fac62ea9
balaji <balaji_penamalli@gmail.com>
2017-03-23 11:07:23 +0530

    Added eureka sysout

--- src/main/java/com/balaji/ms/registry/main/EurekaServiceRegistry.java
+++ src/main/java/com/balaji/ms/registry/main/EurekaServiceRegistry.java
@@ -11,7 +11,7 @@ public class EurekaServiceRegistry {
 	public static void main(String[] args) {
 		
 		System.setProperty("spring.config.name", "service-registry");
-		
+		System.out.println("Balaji Penamalli-Created a new Eureka Server.");
 		SpringApplication.run(EurekaServiceRegistry.class, args);
 
 	}
